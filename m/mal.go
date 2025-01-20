bb7c638c6f78eeedef6b2f8c073d26957c6e4317
M09Ic <m09ician@gmail.com>
2025-01-20 16:06:17 +0800

    feat: move mal parser to mals

---
+++ m/mal.go
@@ -0,0 +1,46 @@
+package m
+
+import (
+	"net/url"
+	"time"
+)
+
+var (
+	MalIndexFileName = "mals.yaml"
+	ManifestFileName = "mal.yaml"
+	DefaultMalName   = "Default"
+
+	//malIndexSigFileName = "mal.minisig"
+)
+
+type MalsYaml struct {
+	Mals []*MalConfig `yaml:"mals"`
+}
+
+type MalConfig struct {
+	RepoURL          string `yaml:"repo_url"`
+	Authorization    string `yaml:"authorization"`
+	AuthorizationCmd string `yaml:"authorization_cmd"`
+	Name             string `yaml:"name"`
+	Enabled          bool   `yaml:"enabled"`
+	Version          string `yaml:"version"`
+	Help             string `yaml:"help"`
+}
+
+// MalHTTPConfig - Configuration for armory HTTP client
+type MalHTTPConfig struct {
+	MalConfig            *MalConfig
+	IgnoreCache          bool
+	ProxyURL             *url.URL
+	Timeout              time.Duration
+	DisableTLSValidation bool
+}
+
+var DefaultMalRepoURL = "https://api.github.com/repos/chainreactors/mals/releases"
+
+var DefaultMalConfig = &MalConfig{
+	//PublicKey: DefaultArmoryPublicKey,
+	RepoURL: DefaultMalRepoURL,
+	Name:    DefaultMalName,
+	Enabled: true,
+}
